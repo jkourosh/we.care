dfdc6e1f14405b041de0269bc67b3486b8bc0ec9
jkourosh <jkourosh@gmail.com>
2022-05-11 12:08:05 +0430

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
+const SelectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if (element) return element;
+    throw new Error(`${element} not found`);
+    
+}
 
+const openTab = (evt,tabName) => {
+    //document.getElementById('Cond').style.display = 'flex';
+    // const tabContent = document.getElementsByClassName('popular__content');
+    // for (let index = 0; index < tabContent.length; index++) {
+    //     tabContent[index].style.display = 'none';
+        
+    // }
+    // console.log(tabName);
+    //document.getElementById(cityName).style.display = "block";
+    // document.getElementById('Cond').style.display = "flex";
+    evt.currentTarget.className += " active";
+}
